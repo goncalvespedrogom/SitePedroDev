f4ba0b9baf9cdab54b80c5845a25a5abca04af1c
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2024-08-26 12:19:45 -0300

    Add scroll top button

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
+let scroll = document.querySelector('.scroll-top')
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
@@ -13,3 +14,14 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+window.addEventListener('scroll', function(){
+    scroll.classList.toggle('active', window.scrollY > 450)
+})
+
+function backTop() {
+    window.scrollTo( {
+        top: 0,
+        behavior: 'smooth'
+    })
+}
